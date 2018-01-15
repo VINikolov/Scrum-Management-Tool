044104221493e3c0270c4fe54892dce6602d1204
Vasil Nikolov <v.nikolov.95@gmail.com>
2018-01-16 00:46:17 +0200

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import LoginForm from './LoginForm'
+
+ReactDOM.render(<LoginForm />, document.getElementById('root'));
