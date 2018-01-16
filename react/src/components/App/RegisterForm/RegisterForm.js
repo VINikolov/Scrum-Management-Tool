74c1e04dc6c577e9c983fd80d9514ab11d10d367
unknown <v.nikolov.95@gmail.com>
2018-01-16 23:48:12 +0200

    Implementation of login and register forms.

--- react/src/components/App/RegisterForm/RegisterForm.js
+++ react/src/components/App/RegisterForm/RegisterForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './RegisterForm.css'
+import InputField from '../InputField'
+import FormButton from '../FormButton'
 
 class RegisterForm extends Component {
     render() {
@@ -7,6 +9,14 @@ class RegisterForm extends Component {
             <div className="reg-form">
                 <div className="heading">
                     <h2>Register</h2>
+                    
+                    <form method="POST">
+                        <InputField fieldType="text" fieldPlaceholder="username"/>
+                        <InputField fieldType="password" fieldPlaceholder="password"/>
+                        <InputField fieldType="password" fieldPlaceholder="confirm password"/>
+                        <InputField fieldType="text" fieldPlaceholder="email"/>
+                        <FormButton buttonText="Sign up"/>
+                    </form>
                 </div>
                 
             </div>
---
+++ react/src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './NavigationBar.css'
+
+class NavigationBar extends Component {
+    
+}
+
+export default NavigationBar;
